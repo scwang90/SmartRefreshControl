deda1a4a36ab54a69bda927502f2711674d69108
scwang <scwang90@hotmail.com>
2021-11-04 21:09:38 +0800

    Initial Commit

---
+++ SmartRefresh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartRefresh
+//
+//  Created by SCWANG on 2021/11/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
