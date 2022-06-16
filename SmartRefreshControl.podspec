2477aa8e8970826b2ae67996561bb22ff6da156d
树朾 <scwang90@hotmail.com>
2022-06-16 11:30:46 +0800

    0.0.5

--- SmartRefreshControl.podspec
+++ SmartRefreshControl.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |spec|
   #
 
   spec.name         = "SmartRefreshControl"
-  spec.version      = "0.0.3"
+  spec.version      = "0.0.4"
   spec.summary      = "SmartRefresh for IOS"
 
   # This description is used to generate tags and improve search results.
--- SmartRefreshControl/SmartRefreshControl.h
+++ SmartRefreshControl/SmartRefreshControl.h
@@ -18,48 +18,30 @@ FOUNDATION_EXPORT const unsigned char SmartRefreshControlVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SmartRefreshControl/PublicHeader.h>
 
-// #import <SmartRefreshControl/UIRefreshFooter.h>
-// #import <SmartRefreshControl/UIRefreshHeader.h>
-// #import <SmartRefreshControl/UIRefreshComponent.h>
-
-// #import <SmartRefreshControl/UIRefreshClassicsHeader.h>
-// #import <SmartRefreshControl/UIRefreshTaurusHeader.h>
-// #import <SmartRefreshControl/UIRefreshPhoenixHeader.h>
-// #import <SmartRefreshControl/UIRefreshDropBoxHeader.h>
-// #import <SmartRefreshControl/UIRefreshDeliveryHeader.h>
-// #import <SmartRefreshControl/UIRefreshBezierRadarHeader.h>
-// #import <SmartRefreshControl/UIRefreshBezierCircleHeader.h>
-// #import <SmartRefreshControl/UIRefreshOriginalHeader.h>
-// #import <SmartRefreshControl/UIRefreshMaterialHeader.h>
-// #import <SmartRefreshControl/UIRefreshWaveSwipeHeader.h>
-// #import <SmartRefreshControl/UIRefreshStoreHouseHeader.h>
-// #import <SmartRefreshControl/UIRefreshGameHitBlockHeader.h>
-// #import <SmartRefreshControl/UIRefreshGameBattleCityHeader.h>
-// #import <SmartRefreshControl/UIRefreshFlyHeader.h>
-
-// #import <SmartRefreshControl/UIRefreshClassicsFooter.h>
-
+#if __has_include(<SmartRefreshControl/UIRefreshFooter.h>)
 #import "UIRefreshFooter.h"
-#import "UIRefreshHeader.h"
-#import "UIRefreshComponent.h"
+#else
+#import <SmartRefreshControl/UIRefreshFooter.h>
+#endif
+#import <SmartRefreshControl/UIRefreshHeader.h>
+#import <SmartRefreshControl/UIRefreshComponent.h>
 
-#import "UIRefreshClassicsHeader.h"
-#import "UIRefreshTaurusHeader.h"
-#import "UIRefreshPhoenixHeader.h"
-#import "UIRefreshDropBoxHeader.h"
-#import "UIRefreshDeliveryHeader.h"
-#import "UIRefreshBezierRadarHeader.h"
-#import "UIRefreshBezierCircleHeader.h"
-#import "UIRefreshOriginalHeader.h"
-#import "UIRefreshMaterialHeader.h"
-#import "UIRefreshWaveSwipeHeader.h"
-#import "UIRefreshStoreHouseHeader.h"
-#import "UIRefreshGameHitBlockHeader.h"
-#import "UIRefreshGameBattleCityHeader.h"
-#import "UIRefreshFlyHeader.h"
+#import <SmartRefreshControl/UIRefreshClassicsHeader.h>
+#import <SmartRefreshControl/UIRefreshTaurusHeader.h>
+#import <SmartRefreshControl/UIRefreshPhoenixHeader.h>
+#import <SmartRefreshControl/UIRefreshDropBoxHeader.h>
+#import <SmartRefreshControl/UIRefreshDeliveryHeader.h>
+#import <SmartRefreshControl/UIRefreshBezierRadarHeader.h>
+#import <SmartRefreshControl/UIRefreshBezierCircleHeader.h>
+#import <SmartRefreshControl/UIRefreshOriginalHeader.h>
+#import <SmartRefreshControl/UIRefreshMaterialHeader.h>
+#import <SmartRefreshControl/UIRefreshWaveSwipeHeader.h>
+#import <SmartRefreshControl/UIRefreshStoreHouseHeader.h>
+#import <SmartRefreshControl/UIRefreshGameHitBlockHeader.h>
+#import <SmartRefreshControl/UIRefreshGameBattleCityHeader.h>
+#import <SmartRefreshControl/UIRefreshFlyHeader.h>
 
-#import "UIRefreshClassicsFooter.h"
-
+#import <SmartRefreshControl/UIRefreshClassicsFooter.h>
 
 #else
 
